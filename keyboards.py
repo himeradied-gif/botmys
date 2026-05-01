01fec2775064685fd754a65ad1cea19ab4216bf9
himeradied-gif <himera.died@gmail.com>
2026-05-01 21:06:46 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,16 +3,22 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def main_menu():
     buttons = [
         [InlineKeyboardButton(text="📋 Профиль", callback_data="profile")],
+        [InlineKeyboardButton(text="💰 Мой баланс", callback_data="balance")],
         [InlineKeyboardButton(text="✅ РАБОТАЮ", callback_data="work_today")],
-        [InlineKeyboardButton(text="ℹ️ Моя услуга", callback_data="my_service")]
+        [
+            InlineKeyboardButton(text="📅 Статистика", callback_data="stats"),
+            InlineKeyboardButton(text="ℹ️ Правила", callback_data="help")
+        ],
+        [InlineKeyboardButton(text="🛠 Поддержка", callback_data="support")]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
+
 def category_keyboard():
     buttons = [
-        [InlineKeyboardButton(text="1️⃣ Электросамокаты / велосипеды (3.4$/день)", callback_data="cat_1")],
-        [InlineKeyboardButton(text="2️⃣ Повербанки в аренду (2$/день)", callback_data="cat_2")],
-        [InlineKeyboardButton(text="3️⃣ Крипто-майнинг фермы (4$/день)", callback_data="cat_3")],
-        [InlineKeyboardButton(text="4️⃣ Недвижимость посуточно (2.8$/день)", callback_data="cat_4")]
+        [InlineKeyboardButton(text="1️⃣ Электросамокаты / велосипеды — 3.4$/день", callback_data="cat_1")],
+        [InlineKeyboardButton(text="2️⃣ Повербанки в аренду — 2$/день", callback_data="cat_2")],
+        [InlineKeyboardButton(text="3️⃣ Крипто-майнинг фермы — 4$/день", callback_data="cat_3")],
+        [InlineKeyboardButton(text="4️⃣ Недвижимость посуточно — 2.8$/день", callback_data="cat_4")]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
